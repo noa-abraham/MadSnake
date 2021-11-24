db27e54d71ea2608df184026c59f59a50382b0d5
noa-abraham <noeliasabraham@gmail.com>
2021-11-24 01:19:16 -0300

    Más clases

--- src/main/java/juegoCalamar/Tablero.java
+++ src/main/java/juegoCalamar/Tablero.java
@@ -1,15 +1,23 @@
 package juegoCalamar;
 
-public interface Tablero extends Dibujable {
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Tablero extends ElementoBasico {
 	
-	public int getPosicionX();
 	
-	public int getPosicionY();
-	public int getAncho (); 
-	public int getLargo (); 
-	public Color getColor(); 
-	public void moverse ();
-	public boolean hayColision (Tablero tablero); 
-	
+	public Tablero(int posicionX, int posicionY, double velocidadX, double velocidadY, int ancho, int largo,
+            Color color) {
+        super(posicionX, posicionY, velocidadX, velocidadY, ancho, largo, color);
+    }
+
+    public void dibujarse(Graphics graphics) { //dibuja el puente
+        graphics.setColor(getColor());
+        graphics.fillRect(getPosicionX(), getPosicionY(), getAncho(), getLargo());
+    }
+    
+    public void destruirse (Graphics graphic) { //cuando se pierde
+    	
+    }
 
 }
