02f2159c82c33a4061597fddc92b43abedfcba07
noa-abraham <noeliasabraham@gmail.com>
2021-12-09 16:44:20 -0300

    Borrando pruebas de imágenes

--- src/main/java/juegoViborita/ArrancarJuego.java
+++ src/main/java/juegoViborita/ArrancarJuego.java
@@ -8,15 +8,15 @@ public class ArrancarJuego {
     public static void main(String[] args) {
 
         // Propiedades del Juego
-        int anchoVentana = 600;
-        int largoVentana = 640;
+        int anchoVentana = 905;
+        int largoVentana = 700;
         int tiempoDeEsperaEntreActualizaciones = 80;
         
      // Activar aceleracion de graficos en 2 dimensiones
         System.setProperty("sun.java2d.opengl", "true");
 
         // Crear un objeto de tipo JFrame que es la ventana donde va estar el juego
-        JFrame ventana = new JFrame("****S U P E R // S N I K E**** ");
+        JFrame ventana = new JFrame("****M A D  // S N I K E**** ");
 
         // Cerrar la aplicacion cuando el usuario hace click en la 'X'
         ventana.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
