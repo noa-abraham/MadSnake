eef6d070d30d23dd1e96198d1e239fec98c5a221
alangim <82983554+kartucho@users.noreply.github.com>
2021-12-16 23:57:10 -0300

    pude con pararSonido

--- src/main/java/juegoViborita/Sonidos.java
+++ src/main/java/juegoViborita/Sonidos.java
@@ -15,9 +15,11 @@ import javax.sound.sampled.DataLine;
 public class Sonidos {
 
     private Map<String, byte[]> sonidos;
+    private Map<String, Clip> clips;
 
     public Sonidos() {
         this.sonidos = new HashMap<String, byte[]>();
+        clips = new HashMap<>();
     }
 
     public void agregarSonido(String nombre, String archivo) {
@@ -39,17 +41,31 @@ public class Sonidos {
 
     private void tocarSonido(String sonido, boolean repetir) {
         try {
-            byte[] sonidoEnBytes = sonidos.get(sonido);
-            InputStream myInputStream = new ByteArrayInputStream(sonidoEnBytes);
-            AudioInputStream ais = AudioSystem.getAudioInputStream(myInputStream);
-            DataLine.Info info = new DataLine.Info(Clip.class, ais.getFormat());
-            Clip clip = (Clip) AudioSystem.getLine(info);
-            clip.open(ais);
-            if (repetir) {
-            	clip.loop(Clip.LOOP_CONTINUOUSLY);
-            } else {
-                clip.start();
-            }
+        	if(!clips.containsKey(sonido)) {
+        		byte[] sonidoEnBytes = sonidos.get(sonido);
+                InputStream myInputStream = new ByteArrayInputStream(sonidoEnBytes);
+                AudioInputStream ais = AudioSystem.getAudioInputStream(myInputStream);
+                DataLine.Info info = new DataLine.Info(Clip.class, ais.getFormat());
+                Clip clip = (Clip) AudioSystem.getLine(info);
+                clip.open(ais);
+                if (repetir) {
+                	clip.loop(Clip.LOOP_CONTINUOUSLY);
+                	clips.put(sonido, clip);
+                } else {
+                    clip.start();
+                }
+        	}
+        } catch (Exception e) {
+        	e.printStackTrace();
+        }
+    }
+    
+    //Para sonido y lo elimina del mapa
+    public void pararSonido(String sonido) {
+        try {
+        	Clip clip = clips.get(sonido);
+        	clip.stop();
+        	clips.remove(sonido);
         } catch (Exception e) {
         	e.printStackTrace();
         }
