4a699805585fea7383a2c933f468fc0c88c55e05
Noelia Abraham <noeliasabraham@gmail.com>
2022-07-17 10:51:45 -0300

    Update Nivel.java

--- src/main/java/juegoViborita/Nivel.java
+++ src/main/java/juegoViborita/Nivel.java
@@ -12,25 +12,23 @@ public class Nivel implements Dibujable {
 	 private Color color;
 	 private int nivel; 
 
-    public Nivel (int posicionX, int posicionY, Font font, Color color) {
-    	this.posicionX = posicionX;
-        this.posicionY = posicionY;
-        this.font = font;
-        this.color = color;
-        this.nivel = 1;
-    }
+    	public Nivel (int posicionX, int posicionY, Font font, Color color) {
+    		this.posicionX = posicionX;
+        	this.posicionY = posicionY;
+        	this.font = font;
+        	this.color = color;
+        	this.nivel = 1;
+    	}
+	
+    	@Override
+    	public void dibujarse(Graphics g) {
+        	g.setColor(color);
+       	 	g.setFont(font);
+        	g.drawString("Nivel: " + Juego.level, posicionX, posicionY);
+    	}
 
-    
-    @Override
-    public void dibujarse(Graphics g) {
-        g.setColor(color);
-        g.setFont(font);
-        g.drawString("Nivel: " + Juego.level, posicionX, posicionY);
-    }
-    
-    
-    public int getNivel() {
-        return nivel;
-    }
+    	public int getNivel() {
+        	return nivel;
+    	}
     
 }
